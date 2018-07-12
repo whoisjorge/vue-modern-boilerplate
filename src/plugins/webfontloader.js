cc8fa483b2f296958a5e0ceb64fc985e62d2b596
Jorge Moliner <email@whoisjorge.me>
2018-07-12 19:16:22 +0200

    import fonts manually || webfontloader

--- src/plugins/webfontloader.js
+++ src/plugins/webfontloader.js
@@ -1,10 +1,6 @@
 const WebFontLoader = require('webfontloader')
 
 WebFontLoader.load({
-  custom: {
-    families: ['Custom-Font'],
-    urls: ['fonts/import.css']
-  },
   google: {
     families: [
       'Overpass:300,600,700',
