cd13b2beeac0bfe62967d9c4b35b4a13f2c9a5ef
Jorge Moliner <email@whoisjorge.me>
2018-07-23 02:38:35 +0200

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -16,9 +16,15 @@ Vue.use(Router)
 Vue.use(VueMeta, { keyName: 'head' })
 
 const router = new Router({
-  routes,
+  // The base URL of the app.
+  base: '/',
   // Use the HTML5 history API instead of routes with hashes.
   mode: 'history',
+  // Configure the active CSS class applied when the link is active.
+  linkActiveClass: 'router--active',
+  linkExactActiveClass: 'router--exact-active',
+  // RouteConfig
+  routes,
   // Force the scroll position to the top for every route.
   // It simulates native-like scroll behavior when navigating to a new
   // route and using back/forward buttons.
